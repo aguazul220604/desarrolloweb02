7aeb8ecf2b850712b56ed2ad8ad24fc10256a771
aguazul220604 <garciaguazul12@gmail.com>
2025-06-10 10:09:47 -0600

    proyecto angular 20

---
+++ ventas_c/src/app/pages/products/products.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Product } from '../../services/products';
+@Component({
+  selector: 'app-products',
+  imports: [],
+  templateUrl: './products.html',
+  styleUrl: './products.css',
+})
+export class Products {}
